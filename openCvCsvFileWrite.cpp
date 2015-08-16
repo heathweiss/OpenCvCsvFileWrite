75f18cfc3a1886848439173a626c9e8288045981
Heath Weiss <heathweiss@hotmail.com>
2015-08-16 05:22:58 -0500

    ready to make attoparsec version

--- openCvCsvFileWrite.cpp
+++ openCvCsvFileWrite.cpp
@@ -1,13 +1,143 @@
+
 #include <fstream>
 #include <sstream> 
 #include <opencv2/opencv.hpp>
 //#include <iostream> 
 //#include <string> 
 
+
 using namespace cv;
+//func declarations for mininum data
+int getMinSingle(Mat image, int row);
+void getMinAll(Mat image, int* mins);
+std::string convertMinAllRowsToString();
+int writeMinStringsToFile (std::string inString,const char* fileName);
 
 
-int getMinLoc(Mat image, int row)
+//func declarations for full data
+void viewSingleRow (int row);
+void writeFullDataToFile();
+void writeFullDataWithDegreesToFile();
+
+int main ()
+{
+ 
+  //writeMinStringsToFile(convertMinAllRowsToString(),"mins.txt");
+  //viewSingleRow (100);
+  //writeFullDataToFile();
+  writeFullDataWithDegreesToFile();
+ 
+ return 0;
+}
+
+/////////////////////////////////////// functions for full data ////////////////////////////////////
+
+/*
+Writes all the image data to file, with multiple images to represent scans at each target degree.
+Sends info about what degree, as the first int of every row, for each of the degree datasets, instead of having it separated.
+Put in on every row to make parsing easier.
+*/
+void writeFullDataWithDegreesToFile()
+{ Mat img = imread("backScratcherFlipped.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+  std::filebuf fb;
+  fb.open ("fullDataWithDegrees.txt",std::ios::out);
+  std::ostream os(&fb);
+
+  //need to send the data 5 times for a simulated 0,90,180,270,360 degree scan.
+  //send it 1st time alone, so it does not have a $ appended to it
+
+  for (int row = 0; row < img.size().height; row++)
+    { if (row > 0) os << ";";
+      os << "0 "; //append degree info
+      for (int col = 0; col < img.size().width; col++)
+  	{ int pixel = img.at<uchar>(row,col);
+          if (col > 0) os << " "; 
+          os << pixel;
+        }
+    }
+
+  //now send it 4 more times with $ appended to it
+  for (int scan = 1; scan <5; scan++)
+    { os << "$";
+
+      for (int row = 0; row < img.size().height; row++)
+        { if (row > 0) os << ";";
+          if (scan == 1) os << "90 "; //append degree info
+          if (scan == 2) os << "180 "; //append degree info
+          if (scan == 3) os << "270 "; //append degree info
+          if (scan == 4) os << "360 "; //append degree info
+          for (int col = 0; col < img.size().width; col++)
+            { int pixel = img.at<uchar>(row,col);
+              if (col > 0) os << " "; 
+              os << pixel;
+            }
+        }
+    }
+    
+  fb.close();
+
+}
+
+
+/*
+Writes all the image data to file, with multiple images to represent scans at each target degree.
+Does not send info about what degree.
+*/
+void writeFullDataToFile()
+{ Mat img = imread("backScratcherFlipped.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+  std::filebuf fb;
+  fb.open ("fullData.txt",std::ios::out);
+  std::ostream os(&fb);
+
+  //need to send the data 5 times for a simulated 0,90,180,270,360 degree scan.
+  //send it 1st time alone, so it does not have a $ appended to it
+
+  for (int row = 0; row < img.size().height; row++)
+    { if (row > 0) os << ";";
+      for (int col = 0; col < img.size().width; col++)
+  	{ int pixel = img.at<uchar>(row,col);
+          if (col > 0) os << " "; 
+          os << pixel;
+        }
+    }
+
+  //now send it 4 more times with $ appended to it
+  for (int scan = 1; scan <5; scan++)
+    { os << "$";
+
+      for (int row = 0; row < img.size().height; row++)
+        { if (row > 0) os << ";";
+          for (int col = 0; col < img.size().width; col++)
+            { int pixel = img.at<uchar>(row,col);
+              if (col > 0) os << " "; 
+              os << pixel;
+            }
+        }
+    }
+    
+  fb.close();
+
+}
+
+
+void viewSingleRow (int row)
+{
+  Mat imageRaw = imread("backScratcherFlipped.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+  cv::Mat imageBW = imageRaw > 45;
+  std::ostringstream os;
+  for (int col = 0; col < imageBW.size().width; col++)
+    { int pixel = imageBW.at<uchar>(row,col);
+      os <<  pixel;
+      os << " ";
+     
+    }
+  std::cout << os.str();
+}
+
+//////////////////////////////////////// functions for min values //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+int getMinSingle(Mat image, int row)
 {
   uchar minVal = 255;
   int minLoc = 0;
@@ -17,7 +147,7 @@ int getMinLoc(Mat image, int row)
   for (int col = 0; col < (image.size().width - 1); col++)
     {
       pixel = image.at<uchar>(row,col);
-      if (pixel < minVal)
+      if (pixel <= (minVal))
 	{
           minVal = pixel;
           minLoc = col;
@@ -26,28 +156,32 @@ int getMinLoc(Mat image, int row)
   return minVal;
 }
 
-void getMinLocations(Mat image, int* mins)
+
+void getMinAllRows(Mat image, int* mins)
 {
   std::cout << "width: [" << image.size().width << "]" << std::endl;
   std::cout << "height: [" << image.size().height << "]" << std::endl;
   for (int row = 0; row < image.size().height; row++)
     {
-      mins[row] = getMinLoc(image,row);
+      mins[row] = getMinSingle(image,row);
     }
   
 }
 
-std::string stringOfMinLocations()
+
+std::string convertMinAllRowsToString()
 {
    //load the image
    //had to flip the image with imageView. WTF? Once I get permanent camera, should change.
    Mat image;
    //image = imread("LinuxLogo.jpg", CV_LOAD_IMAGE_GRAYSCALE );
    image = imread("backScratcherFlipped.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+   //change to black and white, with values about the threshhold being white
+   //cv::Mat image = imageAsRead > 25;
    std::ostringstream os;
    int mins[image.size().height -1];
 
-   getMinLocations(image, mins);
+   getMinAllRows(image, mins);
 
    for (int i = 0; i < (sizeof(mins)/sizeof(mins[0])); i++)
      {
@@ -61,7 +195,7 @@ std::string stringOfMinLocations()
 
 //takes a string, and writes it to a file via a filebuf
 //now add  second/3rd arrays that would represent more angles. Use ; as a sep.
-int writeMinLocationsToFile (std::string inString,const char* fileName)
+int writeMinStringsToFile (std::string inString,const char* fileName)
 {
   std::filebuf fb;
   fb.open (fileName,std::ios::out);
@@ -76,16 +210,4 @@ int writeMinLocationsToFile (std::string inString,const char* fileName)
   
   fb.close();
   return 0;
-}
-
-//////////////////////////////////////// main //////////////////////////////////////
-int main ()
-{
- 
- writeMinLocationsToFile(stringOfMinLocations(),"mins.txt");
- 
- return 0;
-
-
-
 }
